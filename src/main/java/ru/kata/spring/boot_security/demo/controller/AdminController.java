7a93eec7fdccdc8895075e8a18c009054003e42d
Error <ya.gaukler@gmail.com>
2023-12-28 15:29:22 +0300

    v2

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -4,6 +4,7 @@ package ru.kata.spring.boot_security.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 import ru.kata.spring.boot_security.demo.models.User;
@@ -69,31 +70,11 @@ public class AdminController {
         return "redirect:/admin";
     }
 
-    /*@PostMapping("users")
-    public String delete(@RequestParam(value = "id", required = false) Long id) {
-        userService.deleteById(id);
-        return "redirect:/admin";
-    }*/
-    @PostMapping("users/delete")
+    @PostMapping("users")
     public String delete(@RequestParam(value = "id", required = false) Long id) {
         userService.deleteById(id);
         return "redirect:/admin";
     }
 
-   /* private void extracted(Model model) {
-        model.addAttribute("usersList", userService.findAll());
-    }
-    @GetMapping(value = "/form/{id}")
-    public String deleteUserForm(@PathVariable("id") Long id, Model model) {
-        model.addAttribute("user", userService.getById(id));
-        extracted(model);
-        return "delete";
-    }
-
-    @DeleteMapping(value = "/delete/{id}")
-    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
-        userService.deleteById(id);
-        return ResponseEntity.ok().build();
-    }*/
 }
 
